91cff09045d2afd15a6679d089be480159337119
nil8 <nil-8@gmx.com>
2014-08-20 11:08:26 +0200

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This .R file consists of two functions:
+## 1. makeCacheMatrix constructs an abstract matrix object that 
+##    allows to cache the associated inverse matrix.
+## 2. cacheSolve returns the inverse of the matrix represented by
+##    the object created with makeCacheMatrix, and if was not
+##    available yet, caches its answer for future use.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes a single optional argument x, with as 
+## default value the empty matrix. It initializes an 'internal 
+## variable' x, and returns a list of functions 
+## (set, get, setInverse, getInverse) that,respectively, 
+## set the value of x, get the value of x, set a cached value 
+## of the inverse of x, and retrieve cached value of the inverse 
+## of x, if available.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inverse) i <<- inverse
+    getInverse <- function() i
+    list(set = set, get = get, 
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
+## cacheSolve takes as argument a matrix object constructed by
+## makeCacheMatrix, and optional arguments. If a cached matrix 
+## inverse is present, it is returned along with a message. If
+## a cached matrix inverse is not present, it is computed by the
+## solve command along with the optional arguments, and cached 
+## within the matrix object.
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    i <- x$getInverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setInverse(i)
+    i    
 }
